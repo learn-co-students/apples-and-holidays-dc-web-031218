f3e275c29c29fb600bac8a6d0dd3523b3de40687
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 07:20:30 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,17 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter].each do |holiday, arr|
+    arr << supply
+  end
 
 end
 
@@ -31,18 +36,25 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name]= supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  to_return = []
+  holiday_hash[:winter].each do |holiday, arr|
+    arr.each do |item|
+      to_return << item
+    end
+  end
+  to_return
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +65,27 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, data|
+    puts "#{season.capitalize}:"
+    data.each do |holiday, arr|
+      temp = holiday.to_s
+      temp = temp.tr('_', ' ')
+      temp = temp.split.map{|x| x.capitalize}.join(" ")
+      puts "  #{temp}: #{arr.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  to_return =[]
+  holiday_hash.each do |season, data|
+    data.each do |holiday, arr|
+      if arr.include?("BBQ")
+        to_return << holiday
+      end
+    end
+  end
+  to_return
 end
-
-
-
-
-
-
-
